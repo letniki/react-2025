14fb26b017955a0291b1dd12e6805649e06cb349
letniki <nikitaletuta12@gmai.com>
2025-01-02 13:15:33 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyComponent from "./components/MyComponent.tsx";
+
+function App() {
+
+  return (
+      <>
+    <MyComponent text="hello"/>
+      </>
+  )
+}
+
+export default App
