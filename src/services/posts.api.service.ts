11354aee65862a330936e12a4b0885977d7b968f
letniki <nikitaletuta12@gmai.com>
2025-01-09 18:13:38 +0200

    hw3 task1

--- src/services/posts.api.service.ts
+++
@@ -1,6 +0,0 @@
-// const baseUrl = import.meta.env.VITE_BASE_URL;
-//
-// const getPosts= async (): Promise<IPosts>=> {
-//     return await fetch(`${baseUrl}/posts`)
-//         .then(value => value.json)
-// }
--- src/services/users.api.service.ts
+++
@@ -1,6 +0,0 @@
-// const baseUrl = import.meta.env.VITE_BASE_URL;
-
-// const getUsers= async (): Promise<IUsers>=> {
-//     return await fetch(`${baseUrl}/users`)
-//         .then(value => value.json)
-// }
