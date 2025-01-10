229c931cb8d396b7f8002bec48cad50c4c880aed
letniki <nikitaletuta12@gmai.com>
2025-01-10 16:01:49 +0200

    hw3 task3

--- src/components/post-component/PostComponent.tsx
+++ src/components/post-component/PostComponent.tsx
@@ -1,12 +1,12 @@
 import {IPost} from "../../models/IPost.ts";
-
+import './PostComponent.css'
 interface IPostProps {
     post: IPost;
 }
 
 export const PostComponent = ({post} :IPostProps) => {
     return (
-        <div>
+        <div className="block">
             <h2>{post.id}. {post.title}</h2>
             <h3>Likes: {post.reactions.likes}</h3>
             <h3>Dislikes: {post.reactions.dislikes}</h3>
