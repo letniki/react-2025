f7e95a62815900bbdd2b684b84ccb46b63af13bf
letniki <nikitaletuta12@gmai.com>
2025-01-10 15:45:07 +0200

    hw3 task3

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,9 +6,12 @@ import {CartsPage} from "../pages/CartsPage.tsx";
 export const routes =createBrowserRouter([{
     path:'/', element:<MainLayout/>,
     children:[
-        {path:'users', element:<UsersPage/>, children:[
-            {path:':id/carts', element:<CartsPage/>}
-            ]},
+        {
+            path:'users', element:<UsersPage/>
+        },
+        {
+        path:'users/:id/carts', element:<CartsPage/>
+        }
 
     ]
 }])
