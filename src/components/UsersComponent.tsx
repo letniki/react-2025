d97fa89fb736e0f28c88f655654a23b815fb6dba
letniki <nikitaletuta12@gmai.com>
2025-01-24 15:05:05 +0200

    hw6 task1-useCallback

--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,15 +1,21 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 export const UsersComponent = () => {
     console.log('users');
     const [users, setUsers] = useState([]);
 
+    const arr: number[] = useMemo(()=>{
+       return [1, 2, 3, 4, 5];
+    }, []);
+    // буде виконана функція яка поверне в точку arr масив [1, 2, 3, 4, 5]
+    // і він буде мемомізований (закешований)
+
     const foo = useCallback(() =>{
         console.log('test');
     }, [])
     // за допомогою useCallback цю функцію закешували, ця функція відпрацює один раз.
-    // Ще один раз UserComponent не відпрацьовує
+    // Ще один раз UserComponent не відпрацьовує.
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(value => value.json())
@@ -21,7 +27,7 @@ export const UsersComponent = () => {
     return (
         <div>
             users component
-            <UserComponent foo={foo}/>
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
